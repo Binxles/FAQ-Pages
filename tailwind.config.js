541cfd64a8e7d96e604b1b4eb500e3f8febc626d
lymarrie <lucmarrie1@gmail.com>
2021-11-11 11:15:32 -0500

    font + mobile optimization

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
       // => @media (min-width: 640px) { ... }
       'md': '768px',
       // => @media (min-width: 768px) { ... }
-      'lg': '1024px',
+      'lg': '1117px',
       // => @media (min-width: 1024px) { ... }
       'xl': '1280px',
       // => @media (min-width: 1280px) { ... }
